c8197f22539021306d358db554e536df03258973
Riccardo <riccardovigano21@gmail.com>
2026-03-20 10:14:53 +0100

    aggiunto main di test per le classi concessionario, auto, veicoli, moto

---
+++ esercizi_21_03_26/concessionario/Main.java
@@ -0,0 +1,14 @@
+import java.time.Year;
+import java.util.ArrayList;
+/*
+public class Main {
+    public static void main() {
+        ArrayList<Veicoli> concessionarioVeicoli = new ArrayList<>(5);
+
+        Auto auto1 = new Auto("Fiat Panda", 10000, Year.of(2023),"Disel","ford-fiesta");
+        concessionarioVeicoli.add(auto1);
+        Concessionario conc = new Concessionario("Rossi", "via Marino 22 Milano");
+
+    }
+}
+*/
