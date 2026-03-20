aab3ef896424244bf892a344403d34de99837c8f
Riccardo <riccardovigano21@gmail.com>
2026-03-20 10:36:18 +0100

    Cambio dei modificatori di accesso a private Creazione di un costruttore per la classe padre veicoli impostazione dei metodi getter: getPrezzo_base getAnno_immatricolazione get_Costo_assicurazione

--- esercizi_21_03_26/concessionario/Veicoli.java
+++ esercizi_21_03_26/concessionario/Veicoli.java
@@ -1,14 +1,40 @@
 import java.time.Year;
 
 public abstract class Veicoli {
-    protected String nome_veicolo;
-    protected int prezzo_base;
-    protected Year anno_immatricolazione ;
-    protected int costo_assicurazione;
-    protected String tipo_veicolo;
+    private int prezzo_base;
+    private Year anno_immatricolazione ;
+    private int costo_assicurazione;
+    private String tipo_veicolo;
+
+
+
+    // voglio creare un veicolo senza usare classe padre
+
+    public Veicoli(int prezzo_base, Year anno_immatricolazione, int costo_assicurazione) {
+        this.prezzo_base = prezzo_base;
+        this.anno_immatricolazione = anno_immatricolazione;
+        this.costo_assicurazione = costo_assicurazione;
+    }
 
 
     // metodi
+
+    // metodo per calcolare il prezzo getter
+
+    public int getPrezzo_base(){
+        return  prezzo_base;
+    }
+
+    public Year getAnno_immatricolazione() {
+        return  anno_immatricolazione;
+    }
+
+
+    public int getCosto_assicurazione() {
+        return costo_assicurazione;
+    }
+
+
 
 
 
