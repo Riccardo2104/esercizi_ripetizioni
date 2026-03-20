62ce636b00837f64e61517ffa842bbb6ea12003b
Riccardo <riccardovigano21@gmail.com>
2026-03-20 12:57:52 +0100

    scrittura ovveride del metodo getvalore nella classe Moto

--- esercizi_21_03_26/concessionario/Moto.java
+++ esercizi_21_03_26/concessionario/Moto.java
@@ -1,46 +1,27 @@
 import java.time.Year;
-
 public class Moto extends Veicoli {
     private String nome_modello;
     int tasso_deprezzamento = 5 ;
-    public Moto(String nome_veicolo, int prezzo_base, Year anno_immatricolazione, int costo_assicurazione, String tipo_veicolo, String nome_modello , int tasso_deprezzamento){
-        this.nome_veicolo = nome_veicolo;
-        this.prezzo_base = prezzo_base;
-        this.anno_immatricolazione = anno_immatricolazione;
-        this.costo_assicurazione = costo_assicurazione;
-        this.tipo_veicolo = tipo_veicolo;
-        this.nome_modello =nome_modello;
-        this.tasso_deprezzamento = tasso_deprezzamento;
+    public Moto(int valore, Year anno_immatricolazione, String targa, String nome_modello){
+
+        super(valore,anno_immatricolazione,targa, nome_modello);
 
     }
-    @Override
-    public int calcolo_anni() {
-        int anno_immatricolazione_veicolo = anno_immatricolazione.getValue();
-        Year anno_corrente = Year.now();
-        int anno_corrente_intero = anno_corrente.getValue();
-        return anno_corrente_intero - anno_immatricolazione_veicolo;
 
-    }
+    double valore_pre_deprezzamento = this.getValore();
     @Override
-    public int getPrezzo() {
-        int eta = calcolo_anni();
-        int prezzo_aggiornato = 0;
-        /*$Prezzo = PrezzoBase x (1 - tasso)^{eta}/*/
+    public double getValore() {
+        int eta = this.get_eta();
+        int tasso = tasso_deprezzamento;
+
+        // calcolo del valore
         for (int i = 0; i < eta; i++) {
-            prezzo_aggiornato = prezzo_base - prezzo_base * tasso_deprezzamento;
 
+            valore_pre_deprezzamento = valore_pre_deprezzamento - (valore_pre_deprezzamento * tasso/100);
         }
-        return  prezzo_aggiornato;
+        return  valore_pre_deprezzamento;
+
     }
-    @Override
-    public int costo_assicurazione() {
-        int eta = calcolo_anni();
-        int risultato = 0;
-        risultato = eta * 100;
-        if (risultato > 1000) {
-            return 1000;
-        }
-        return  risultato;
-    }
+
 }
 
