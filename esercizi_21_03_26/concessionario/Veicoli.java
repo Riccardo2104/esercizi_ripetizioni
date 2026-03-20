31cad768c0a0e4b1a3a95fb25d9edcf2637fc522
Riccardo <riccardovigano21@gmail.com>
2026-03-20 13:03:49 +0100

    Cambiato a private gli attributi della classe Veicoli Creato un costruttore con anche il tipo di veicolo per le macchine eletttriche Reinseriti/Riscritti i metodi get_eta, get_tipo, getTarga Scritto il metodo calcolo_costo_assicurazione

--- esercizi_21_03_26/concessionario/Veicoli.java
+++ esercizi_21_03_26/concessionario/Veicoli.java
@@ -5,22 +5,57 @@ public abstract class Veicoli {
     /*siccome la traccia dice esplicitamente che i veicoli hanno un anno di immatricolazione o dovuto considerare
     che abbiamo una targa
      */
-    protected int valore;
-    protected Year anno_immatricolazione;
-    protected String targa;
+    private int valore;
+    private Year anno_immatricolazione;
+    private String targa;
+    private int costo_assicurazione;
+    private String tipo;
 
 
     public Veicoli(int valore, Year anno_immatricolazione, String targa){
         this.valore = valore;
         this.anno_immatricolazione = anno_immatricolazione;
         this.targa = targa;
-    }
 
+
+    }
+    public Veicoli(int valore, Year anno_immatricolazione, String targa, String tipo){
+        this.valore = valore;
+        this.anno_immatricolazione = anno_immatricolazione;
+        this.targa = targa;
+        this.tipo = tipo;
+    }
 
     // metodi
 
     // metodo per calcolare il prezzo getter
 
 
+    public double getValore() {
+        return valore;
+    }
 
+    public Year getAnno_immatricolazione() {
+        return anno_immatricolazione;
+    }
+
+    public int get_eta() {
+        int anno_immatricolazione_veicolo = this.getAnno_immatricolazione().getValue();
+        Year anno_corrente = Year.now();
+        // mi salvo un intero del anno coorente
+        int anno_corrente_intero = anno_corrente.getValue();
+        return anno_corrente_intero - anno_immatricolazione_veicolo;
+    }
+    public String getTarga() {
+        return targa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int calcolo_costo_assicurazione() {
+        int eta = this.get_eta();
+        return eta*100;
+    }
 }
